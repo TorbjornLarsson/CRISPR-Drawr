e54821e1b138b4d8fda2cc3f60a0b98962bacc0b
Torbjorn Larsson <torbjorn.b.g.larsson@gmail.com>
2023-02-24 15:38:04 +0100

    Added command parse

--- CRISPR-Drawr.py
+++ CRISPR-Drawr.py
@@ -2,22 +2,35 @@
 # -*- coding: UTF-8 -*-
 
 # Summary: TBD
-# 
- 
+
 import sys
 import logging
+import click
 import numpy as np
+
+@click.group()
+def greet():
+    pass
+
+@greet.command()
+@click.argument('name')  # add the name argument
+def hello(**kwargs):
+    print('Hello, {0}!'.format(kwargs['name']))
+
+@greet.command()
+@click.argument('name')
+def goodbye(**kwargs):
+    print('Goodbye, {0}!'.format(kwargs['name']))
+
+if __name__ == '__main__':
+    greet()
 
 # We want to catch and log all exceptions to begin with
 try:
-    # Main code 
-    print("-------------------------------------\n")
-    print("CRISPR-Drawr v1 [February, 2023]\n")
-    print("SciLifeLab\n")
-    print("Torbjörn Larsson\n")
-    print("-------------------------------------\n")
-
-except: # catch *all* exceptions
+    print()
+    # Main code
+ 
+except:  # catch *all* exceptions
     e = sys.exc_info()[0]
-    print("Error: %s" % e )
+    print("Error: %s" % e)
     logging.exception("message")
