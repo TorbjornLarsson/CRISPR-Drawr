ca360d13c8d4cbe78e0b9bf215bf8e8b80ed3a5b
TorbjornLarsson <torbjorn.b.g.larsson@gmail.com>
2023-03-06 13:27:55 +0100

    Added sORF fetch.

--- CRISPR-Drawr.py
+++ CRISPR-Drawr.py
@@ -5,8 +5,9 @@
 
 import sys
 import logging
+import requests
 import click
-import numpy as np
+#import numpy as np
 from Bio import SeqIO
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -16,23 +17,43 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 def greet():
     """CRISPR experimental design\n
     Author: Torbjörn Larsson"""
-    pass
 
 @greet.command()
-@click.argument('name')  # add the name argument
+@click.argument('file')  # add the name argument
 def read_fna(**kwargs):
-    filename = '{0}'.format(kwargs['name'])
+    filename = '{0}'.format(kwargs['file'])
     getFASTA_record = SeqIO.read(filename, "fasta")
     print(getFASTA_record.description)
     print("n=[1:20]: ", getFASTA_record.seq[1:20])
 
 @greet.command()
-@click.argument('name')  # add the name argument
+@click.argument('file')  # add the name argument
 def read_gbk(**kwargs):
-    filename = '{0}'.format(kwargs['name'])
+    filename = '{0}'.format(kwargs['file'])
     getGenBank_record = SeqIO.read(filename, "genbank")
     print(getGenBank_record.description)
     print("n=[1:20]: ", getGenBank_record.seq[1:20])
+
+@greet.command()
+@click.option('--strand', default=1, help='strand orientation 1/-1', show_default=True)
+@click.argument('chr')
+@click.argument('start')
+@click.argument('stop')
+def get_sORF(**kwargs):
+    chr = '{0}'.format(kwargs['chr'])
+    start = '{0}'.format(kwargs['start'])
+    stop = '{0}'.format(kwargs['stop'])
+    strand = '{0}'.format(kwargs['strand'])
+
+    server = "https://rest.ensembl.org"
+     ext = "/sequence/region/human/"+chr+":"+start+".."+stop+":"+str(strand)+"?mask=hard"
+    r = requests.get(server+ext, headers={ "Content-Type" : "text/x-fasta"})
+ 
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
+ 
+    print(r.text)
 
 if __name__ == '__main__':
     greet()
