de2d51b777bd5ba61c6e05e0e3012ade55a63c03
TorbjornLarsson <torbjorn.b.g.larsson@gmail.com>
2023-03-07 16:38:55 +0100

    Added file handling, cleaned code.

--- CRISPR-Drawr.py
+++ CRISPR-Drawr.py
@@ -4,11 +4,17 @@
 # Summary: TBD
 
 import sys
+import datetime
 import logging
 import requests
 import click
 #import numpy as np
 from Bio import SeqIO
+
+now = datetime.datetime.now()
+tnow = now.strftime("%y%m%d_%H_%M_%S")
+
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.group(context_settings=CONTEXT_SETTINGS)
@@ -62,6 +68,8 @@ def get_sORF(**kwargs):
 @click.argument('start')
 @click.argument('stop')
 def get_arms(**kwargs):
+    logging.info('get_arms')
+    logging.info(kwargs)
     chr = '{0}'.format(kwargs['chr'])
     start = '{0}'.format(kwargs['start'])
     stop = '{0}'.format(kwargs['stop'])
@@ -71,24 +79,20 @@ def get_arms(**kwargs):
     ext = "/sequence/region/human/"+chr+":"+start+".."+stop+":"+str(strand)\
         +"?expand_5prime="+str(pad)+";expand_3prime="+str(pad)+";mask=hard"
     
-    r = requests.get(server+ext, headers={ "Content-Type" : "text/x-fasta"})
+    r = requests.get(server+ext, headers={ "Content-Type" : "text/plain"})
  
     if not r.ok:
         r.raise_for_status()
         sys.exit()
  
     print(r.text[1:100])
-
+    filename = '_'.join([tnow,'chr',chr,start,stop,'strand',str(strand),'pad',str(pad)])
+    filename = filename+'.fna'
+    print(filename)
+    f = open(filename, 'x')
+    f.write(r.text)
+    f.write('\n')
+    f.close()
 
 if __name__ == '__main__':
     greet()
-
-# We want to catch and log all exceptions to begin with
-try:
-    print()
-    # Main code
- 
-except:  # catch *all* exceptions
-    e = sys.exc_info()[0]
-    print("Error: %s" % e)
-    logging.exception("message")
